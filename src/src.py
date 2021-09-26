882e3e17163b3c718712c33a4aa4a625c392cf28
areesgod <73094827+areesgod@users.noreply.github.com>
2021-09-26 16:56:52 +0600

    Update src.py

--- src/src.py
+++ src/src.py
@@ -1,5 +1,5 @@
 import requests
-
+import json
 #Getting data from requests library 
 r = requests.get(
     "https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=250&sparkline=false",
